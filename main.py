5d727db29d8bb8d5d85a8b7240041d7865dd6220
weronikakr <weronikakrz1@gmail.com>
2026-03-11 23:07:49 +0100

    added neural networks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from models.logreg import logreg
+from models.neural import neural_network
 
 if __name__ == "__main__":
     
@@ -10,5 +11,15 @@ if __name__ == "__main__":
     
     metrics_df = logreg(X_train, y_train, X_test, y_test, n_iter=20)
 
-    metrics_df.to_csv('results/models_metrics.csv', index=False)
+    metrics_df.to_csv('results/models_metrics_log.csv', index=False)
     print(metrics_df.head(3))
+
+    metrics_df_nn = neural_network(
+        X_train.values,
+        y_train,
+        X_test.values,
+        y_test,
+        n_iter=5
+    )
+    metrics_df_nn.to_csv('results/models_metrics_nn.csv', index=False)
+    print(metrics_df_nn.head(3))
